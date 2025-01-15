af3d664520403a0c7c90b5c8b292ea7264c2a52d
maoguala <danny338898@gmail.com>
2025-01-15 15:33:48 +0800

    :)

--- bitwise.cpp
+++ bitwise.cpp
@@ -6,8 +6,8 @@ int main()
     int k = 2;
     int a = 3;
 
-    std::cout << (n << 2) << std::endl; //output 4
-    std::cout << (n >> 2) << std::endl; //output 0 , out of range by rightmovement
+    std::cout << (n << 2) << std::endl; //Left Shift, output 4
+    std::cout << (n >> 2) << std::endl; //Right Shift, output 0 , out of range by rightmovement
     std::cout << (k & a) << std::endl; //AND, output 010(2)
     std::cout << (n | a) << std::endl; //OR, output 011(3)
     std::cout << (k ^ n) << std::endl; //XOR, output 001(1) 
