e2d8660afc1a86b9ca2b29ac69d78c1e095f2446
maoguala <danny338898@gmail.com>
2025-01-15 15:13:03 +0800

    example

---
+++ bitwise.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int n = 1;
+    int k = 2;
+    int a = 3;
+
+    std::cout << (n << 2) << std::endl; //output 4
+    std::cout << (n >> 2) << std::endl; //output 0 , out of range by rightmovement
+    std::cout << (k & a) << std::endl; //AND, output 010(2)
+    std::cout << (n | a) << std::endl; //OR, output 011(3)
+    std::cout << (k ^ n) << std::endl; //XOR, output 001(1) 
+}
